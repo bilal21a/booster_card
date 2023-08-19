2958c73d6fb559ce0ecee50ef27e4e374740d9ce
Bilal Mac <b.anwar21.a@gmail.com>
2023-08-19 18:41:44 +0500

    fav icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Home</title>
+    <link rel="icon" href="{{ asset('imgs/booster_white_small_logo.png') }}" type="image/x-icon">
+
     <link rel='stylesheet' href='{{ asset('css/custom-frontend-lite.min.css') }}' />
     <link rel='stylesheet' href='{{ asset('css/post-7.css') }}' />
     <link rel='stylesheet' href='{{ asset('css/all.min.css') }}' />
